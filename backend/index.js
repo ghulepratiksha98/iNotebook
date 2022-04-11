8fe83e73c11ea280824ff11a1fc6bc498badb850
TIAA USER <ghulepratiksha98@gmail.com>
2022-04-11 11:05:08 +0700

    v-44 creating routes and schema

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => 
-  res.send('Hello World!')
-)
+//Available routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
+// app.get('/', (req, res) => 
+//   res.send('Hello World!')
+// )
+// app.get('/api/v1/login', (req, res) => 
+//   res.send('Hello login!')
+  
+// )
+// app.get('/api/v1/signup', (req, res) => 
+//   res.send('Hello sigbnup!')
+// )
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
