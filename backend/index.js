f586860190e92d389ca2f2e1b788809d4ab351b9
TIAA USER <ghulepratiksha98@gmail.com>
2022-04-11 12:07:50 +0700

    v-45 storing data into database using mond=goose model

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//middleware to access res.body
+app.use(express.json());
+
 //Available routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    obj = {
-        a: 'thios',
-        number: 34
-    }
-    res.json(obj)
+
+//create a user using post: '/api/auth'. doesnt require auth
+
+
+router.post('/', (req, res)=>{
+    // obj = {
+    //     a: 'thios',
+    //     number: 34
+    // }
+    // res.json(obj)
+    console.log(req.body);
+    // res.send("Hello");
+   const user = User(req.body);
+   user.save()
+    res.send(req.body);
 } )
 
 module.exports = router
