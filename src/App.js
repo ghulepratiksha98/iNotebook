becaa73e271c0d5d86212128dd5c5b82b8fb9324
TIAA USER <ghulepratiksha98@gmail.com>
2022-04-12 16:26:09 +0700

    v-56 creaating Navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+
 
 function App() {
   return (
-    <div className="App">
-        <h1>This is iNtebook</h1>
+    <>
+     <div className="App">
+      <h1>iNotebook</h1>
+      <Router>
+        <Navbar />
+
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
+    </>
   );
 }
 
