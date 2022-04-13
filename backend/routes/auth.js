2c6b72a1f563e6976367da19a6dc544ae0a9fc0e
TIAA USER <ghulepratiksha98@gmail.com>
2022-04-14 01:25:15 +0700

    v-70 Adding Signup Componentn

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -13,10 +13,11 @@ body('name', 'Enter a valid name').isLength({ min: 3 }),
 body('email', 'Enter a valid email').isEmail(),
 body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),],
 async(req, res)=>{ 
+  let success = false;
     //If there are errors return bad errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success, errors: errors.array() });
     }
     //check whether the user wih his email exist already
     try {
@@ -24,7 +25,7 @@ async(req, res)=>{
    
     let user = await User.findOne({email: req.body.email});
     if(user){
-        return res.status(400).json({error: "sorry a user with his email already exist"});
+        return res.status(400).json({success, error: "sorry a user with his email already exist"});
     }
     const salt = await bcrypt.genSalt(10);
     secPass = await bcrypt.hash(req.body.password,salt)
@@ -50,7 +51,8 @@ async(req, res)=>{
     const authtoken = jwt.sign(data, JWT_SECRET);
     // console.log(jwtData);
     // res.json (user)
-    res.json({authtoken})
+    success= true;
+    res.json({ success, authtoken})
 } catch (error) {
     console.log(error.message);
     res.status(500).send("some error occured");
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory, useNavigate } from "react-router-dom";
 
 const Signup = () => {
+    const [credentials, setCredentials] = useState({name:"" , email: "", password: "" , cpassword:""})
+    const navigate = useNavigate() 
+    const handleSubmit =  async(e) => {
+        e.preventDefault();
+        const{name,email,password} = credentials;
+        const response = await fetch("http://localhost:5000/api/auth/createUser", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({name:credentials.name, email: credentials.email, password: credentials.password})
+        });
+        const json = await response.json()
+        console.log(json);
+        if (json.success){
+            // Save the auth token and redirect
+            localStorage.setItem('token', json.authtoken); 
+           navigate("/");
+
+        }
+        else{
+            alert("Invalid credentials");
+        }
+    }
+
+    const onChange = (e)=>{
+        setCredentials({...credentials, [e.target.name]: e.target.value})
+    }
   return (
     <div>
-      I am Signup
+      Signup
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+            <label htmlFor="name" className="form-label">Name</label>
+            <input type="text" className="form-control" id="name" name="name" aria-describedby="emailHelp"  onChange={onChange} value={credentials.name}/>
+
+        </div>
+        <div className="mb-3">
+            <label htmlFor="password" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp"  onChange={onChange} value={credentials.email}/>
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+            <label htmlFor="password" className="form-label">Password</label>
+            <input type="password" className="form-control" id="password" name='password' aria-describedby="emailHelp" onChange={onChange}  value={credentials.password} minLength={5} required/>
+
+        </div>
+        <div className="mb-3">
+            <label htmlFor="cpassword" className="form-label">Confirm Password</label>
+            <input type="password" className="form-control" id="cpassword" name='cpassword' aria-describedby="emailHelp" onChange={onChange}  value={credentials.cpassword} minLength={5} required/>
+
+        </div>
+        
+
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
     </div>
   );
 }
